1392a0968f58c993279925dc8c62467025b85e91
AlyanQ <alyanq117@gmail.com>
2024-04-23 17:01:10 +0400

    Recoloring from purple to white

--- src/front/shared/pages/Wallet/CurrenciesList.tsx
+++ src/front/shared/pages/Wallet/CurrenciesList.tsx
@@ -75,7 +75,7 @@ function CurrenciesList(props: CurrenciesListProps) {
               />
             )}
           />
-          <div styleName="addCurrencyBtnWrapper">
+          {/* <div styleName="addCurrencyBtnWrapper">
             <Button id="addCustomTokenBtn" onClick={openAddCustomTokenModal} transparent fullWidth>
               <FormattedMessage id="addCustomToken" defaultMessage="Add custom token" />
             </Button>
@@ -106,7 +106,7 @@ function CurrenciesList(props: CurrenciesListProps) {
                 <FormattedMessage id="addAsset" defaultMessage="Add currency" />
               </Button>
             )}
-          </div>
+          </div> */}
         </>
       )}
       {!showAssets && !metamask.isConnected() && (
